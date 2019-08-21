50520ef0bd1bf10c9a24812f9affb4d0bea0f570
jeff ma <18jeffreyma@gmail.com>
2019-08-20 17:36:00 -0700

    binary labelling + bettter motion artifact simulation

--- synthetic_motion_utility.py
+++ synthetic_motion_utility.py
@@ -1,6 +1,58 @@
 import numpy as np
 import random
 
+def ifftnc(x, axes):
+    tmp = np.fft.fftshift(x, axes=axes)
+    tmp = np.fft.ifftn(tmp, axes=axes)
+    return np.fft.ifftshift(tmp, axes=axes)
+
+def fftnc(x, axes):
+    tmp = np.fft.fftshift(x, axes=axes)
+    tmp = np.fft.fftn(tmp, axes=axes)
+    return np.fft.ifftshift(tmp, axes=axes)
+
+def fftc(x, axis=0, do_orthonorm=True):
+    if do_orthonorm:
+        scale = np.sqrt(x.shape[axis])
+    else:
+        scale = 1.0
+    return fftnc(x, (axis,)) / scale
+
+def ifftc(x, axis=0, do_orthonorm=True):
+    if do_orthonorm:
+        scale = np.sqrt(x.shape[axis])
+    else:
+        scale = 1.0
+    return ifftnc(x, (axis,)) * scale
+
+def fft2c(x, order='C', do_orthonorm=True):
+    if order == 'C':
+        if do_orthonorm:
+            scale = np.sqrt(np.prod(x.shape[-2:]))
+        else:
+            scale = 1.0
+        return fftnc(x, (-2, -1)) / scale
+    else:
+        if do_orthonorm:
+            scale = np.sqrt(np.prod(x.shape[:2]))
+        else:
+            scale = 1.0
+        return fftnc(x, (0, 1)) / scale
+
+def ifft2c(x, order='C', do_orthonorm=True):
+    if order == 'C':
+        if do_orthonorm:
+            scale = np.sqrt(np.prod(x.shape[-2:]))
+        else:
+            scale = 1.0
+        return ifftnc(x, (-2, -1)) * scale
+    else:
+        if do_orthonorm:
+            scale = np.sqrt(np.prod(x.shape[:2]))
+        else:
+            scale = 1.0
+        return ifftnc(x, (0, 1)) * scale
+
 def generate_random_range(start, end, artifact_length=[0,20], gap_length=[0,100], start_ofset=[0,20]):
     
     list = []
@@ -27,7 +79,7 @@ def add_motion_artifact(im, seed=777):
     
     random.seed(a=seed)
     
-    im_transformed = np.fft.fft2(im)
+    im_transformed = fft2c(im)
     vals = generate_random_range(0, 511, artifact_length=[10,60], gap_length=[50,120], start_ofset=[0,50])
     print(vals)
     
@@ -63,8 +115,9 @@ def add_artifact(img, regions=[[0,511]], max_lf=500, seed=777):
 
 def reconstruct(img):
     
-    img = np.absolute(np.fft.ifft2(img))
+    img = np.absolute(ifft2c(img))
 
     img = np.around(img * 255.0 / np.max(img))
     
     return img
+
